9b5614a352b548ae23787577628ab511da4df74c
Andy Lindsay <andydlindsay@gmail.com>
2018-11-27 18:05:15 -0800

    updates chat to send and receive messages over web sockets

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 import Favicon from 'react-favicon';
 
-import data from './data/data.json';
 import { runInThisContext } from 'vm';
 
 class App extends Component {
@@ -12,24 +11,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: data,
-      currentUser: { name: 'MarbleToast' }
+      messages: [],
+      currentUser: { name: 'MarbleToast' },
+      socket: {}
     };
     this.newMessage = this.newMessage.bind(this);
   }
 
+  componentDidMount() {
+    const webSocket = new WebSocket('ws://localhost:3001');
+
+    webSocket.onmessage = ({ data }) => {
+      const oldMessages = this.state.messages;
+      const messages = [...oldMessages, JSON.parse(data) ];
+      this.setState({ messages });
+    };
+    this.setState({ socket: webSocket });
+  }
+
   newMessage(event) {
     event.preventDefault();
-    const username = event.target.elements['username'].value || this.state.currentUser.name;
+    const username = event.target.elements['username'].value;
+    if (username !== this.state.currentUser.name) {
+      const message = {
+        type: 'incomingNotification',
+        content: `${this.state.currentUser.name} has changed their name to ${username}`
+      }
+      this.state.socket.send(JSON.stringify(message));
+      this.setState({ currentUser: { name: username }});
+    }
     const content = event.target.elements['message'].value;
-    const message = {
-      username,
-      content,
-      type: 'incomingMessage'
-    };
-    event.target.elements['message'].value = '';
-    const messages = [...this.state.messages, message];
-    this.setState({ messages })
+    if (content.length > 0) {
+      const message = {
+        username,
+        content,
+        type: 'incomingMessage'
+      };
+      event.target.elements['message'].value = '';
+      this.state.socket.send(JSON.stringify(message));
+    }
   }
 
   render() {
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,7 +4,7 @@ class NavBar extends Component {
     render() {
         return (
             <nav className="navbar">
-                <a href="/" className="navbar-brand">Chatty</a>
+                <a href="/" className="navbar-brand">Chatty Appy</a>
             </nav>
         );
     }
